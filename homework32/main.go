ca636b612d5af43fc7ec6801756d605728c193b7
Abbos Qambarov <abbosqambarov47@gmail.com>
2024-06-05 16:52:40 +0500

    done homework32

---
+++ homework32/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		fmt.Println("Error setting up listener:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Server is listening on port 8080...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn) 
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Client connected:", conn.RemoteAddr().String())
+
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			return
+		}
+
+		fmt.Print("Received message: ", string(message))
+		message = "qabul qilindi"
+		_, err = conn.Write([]byte(strings.ToUpper(message) + "\n")) 
+		if err != nil {
+			fmt.Println("Error writing message:", err)
+			return
+		}
+	}
+}
